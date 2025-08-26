ab2f6606bcb5f67037d617eec0105ea67e20cd40
yogesh <yogeshsaravanan21@gmail.com>
2025-08-26 15:37:20 +0200

    storing the FC within each model

--- PyFMRheo_DyNaMo/src/pyfmrheo/models/hertz.py
+++ PyFMRheo_DyNaMo/src/pyfmrheo/models/hertz.py
@@ -2,20 +2,23 @@ import numpy as np
 from lmfit import Model, Parameters
 
 from .correction_factors import (
-    bec_dimitriadis_paraboloid_bonded, bec_dimitriadis_paraboloid_not_bonded, 
+    bec_dimitriadis_paraboloid_bonded, bec_dimitriadis_paraboloid_not_bonded,
     bec_gavara_cone, bec_managuli_cone, bec_garcia_garcia, sphere_approx_kontomaris
 )
 
 from .geom_coeffs import get_coeff
+
 
 class HertzModel:
     # Model abstract class
     def __init__(self, ind_geom, tip_param, correction_model=None) -> None:
         # Tip geomtry params
         self.ind_geom = ind_geom         # No units
-        self.tip_parameter = tip_param   # If radius units is meters, If half angle units is degrees
+        # If radius units is meters, If half angle units is degrees
+        self.tip_parameter = tip_param
         self.correction_model = correction_model
-        self.fit_method = 'leastsq' #leastsq is the default, Least-Squares minimization, using Trust Region Reflective method
+        # leastsq is the default, Least-Squares minimization, using Trust Region Reflective method
+        self.fit_method = 'leastsq'
         # Compiutation params
         self.fit_hline_flag = False
         self.apply_correction_flag = False
@@ -53,16 +56,18 @@ class HertzModel:
         self.Rsquared = None
         self.chisq = None
         self.redchi = None
-        #this is to store the max indentation of a fitted force curve
+        # this is to store the max indentation of a fitted force curve
         self.max_ind = None
-        #this is to store the POC initial estimate
+        # this is to store the POC initial estimate
         self.z_c = None
-        #this is to store the Z height at setpoint in m
+        # this is to store the Z height at setpoint in m
         self.z_at_setpoint = None
+        self.run_fit_ident = []
+        self.run_fit_force = []
 
-    
     def get_correction_coeffs(self, sample_height, indentation):
-        correction_params = [sample_height, (indentation - self.delta0), self.ind_geom, self.tip_parameter]
+        correction_params = [
+            sample_height, (indentation - self.delta0), self.ind_geom, self.tip_parameter]
         if self.correction_model == 'dimitriadis_paraboloid_bonded':
             self.poisson_ratio = 0.5
             return bec_dimitriadis_paraboloid_bonded(*correction_params)
@@ -81,27 +86,31 @@ class HertzModel:
         else:
             # TO DO: Implement custom exception
             raise Exception('Correction model not implemented')
-    
+
     def build_params(self):
         params = Parameters()
-        params.add('delta0', value=self.delta0_init, min=self.delta0_min, max=self.delta0_max)
+        params.add('delta0', value=self.delta0_init,
+                   min=self.delta0_min, max=self.delta0_max)
         params.add('E0', value=self.E0_init, min=self.E0_min, max=self.E0_max)
         params.add('f0', value=self.f0_init, min=self.f0_min, max=self.f0_max)
         if self.fit_hline_flag:
-            params.add('slope', value=self.slope_init, min=self.slope_min, max=self.slope_max)
+            params.add('slope', value=self.slope_init,
+                       min=self.slope_min, max=self.slope_max)
         return params
 
     def model(self, indentation, delta0, E0, f0, slope=None, sample_height=None):
         # Define output array
         force = np.zeros(indentation.shape)
-        
+
         # Get indenter shape coefficient and exponent
-        coeff, n = get_coeff(self.ind_geom, self.tip_parameter, self.poisson_ratio)
+        coeff, n = get_coeff(
+            self.ind_geom, self.tip_parameter, self.poisson_ratio)
         # Get bottom effect correction coefficients
         if self.correction_model and self.correction_model == 'kontomaris':
             correction_coeffs = self.get_correction_coeffs(None, indentation)
         elif self.correction_model and sample_height:
-            correction_coeffs = self.get_correction_coeffs(sample_height, indentation)
+            correction_coeffs = self.get_correction_coeffs(
+                sample_height, indentation)
         else:
             correction_coeffs = np.ones(indentation.shape)
         # Compute the force using Hertz model
@@ -111,7 +120,8 @@ class HertzModel:
         # Non-contact part
         if self.fit_hline_flag:
             # Fit a line on the non contact part
-            force[non_contact_mask] = (indentation[non_contact_mask] - delta0) * slope + f0
+            force[non_contact_mask] = (
+                indentation[non_contact_mask] - delta0) * slope + f0
         else:
             # Assign f0 as force value
             force[non_contact_mask] = f0
@@ -120,35 +130,41 @@ class HertzModel:
         # Fit Hertz model on the contact part
         # F = F0 * Correction Coefficient
         force[contact_mask] = (
-            coeff * correction_coeffs[contact_mask] * E0 * np.power((indentation[contact_mask] - delta0), n) + f0
+            coeff * correction_coeffs[contact_mask] * E0 *
+            np.power((indentation[contact_mask] - delta0), n) + f0
         )
 
         return force
 
     def fit(self, indentation, force, sample_height=None):
         # If sample height is given, assign sample height
-        #self.run_fit_ident = indentation;self.run_fit_force = force;self.sample_height = sample_height
-        
-        
-        coeff, n = get_coeff(self.ind_geom, self.tip_parameter, self.poisson_ratio)
+        self.run_fit_ident = indentation
+        self.run_fit_force = force
+        self.sample_height = sample_height
+
+        coeff, n = get_coeff(
+            self.ind_geom, self.tip_parameter, self.poisson_ratio)
         self.E0_init = np.max(force) / coeff / np.max(indentation) ** n
         # Param order:
         # delta0, E0, f0, slope
         if self.fit_hline_flag:
             hertzmodel =\
-             lambda indentation, delta0, E0, f0, slope: self.model(indentation, delta0, E0, f0, slope, self.sample_height)
+                lambda indentation, delta0, E0, f0, slope: self.model(
+                    indentation, delta0, E0, f0, slope, self.sample_height)
         else:
             hertzmodel =\
-             lambda indentation, delta0, E0, f0: self.model(indentation, delta0, E0, f0, self.slope, self.sample_height)
-        
+                lambda indentation, delta0, E0, f0: self.model(
+                    indentation, delta0, E0, f0, self.slope, self.sample_height)
+
         hertzmodelfit = Model(hertzmodel)
-        
+
         # Define free params
         params = self.build_params()
-        
+
         # Do fit
         self.n_params = len(hertzmodelfit.param_names)
-        result_hertz = hertzmodelfit.fit(force, params, indentation=indentation, method=self.fit_method)
+        result_hertz = hertzmodelfit.fit(
+            force, params, indentation=indentation, method=self.fit_method)
 
         # Assign fit results to model params
         self.delta0 = result_hertz.best_values['delta0']
@@ -156,42 +172,42 @@ class HertzModel:
         self.f0 = result_hertz.best_values['f0']
         if self.fit_hline_flag:
             self.slope = result_hertz.best_values['slope']
-        
+
         modelPredictions = self.eval(indentation, sample_height)
 
         absError = modelPredictions - force
 
-        self.MAE = np.mean(absError) # mean absolute error
-        self.SE = np.square(absError) # squared errors
-        self.MSE = np.mean(self.SE) # mean squared errors
-        self.RMSE = np.sqrt(self.MSE) # Root Mean Squared Error, RMSE
+        self.MAE = np.mean(absError)  # mean absolute error
+        self.SE = np.square(absError)  # squared errors
+        self.MSE = np.mean(self.SE)  # mean squared errors
+        self.RMSE = np.sqrt(self.MSE)  # Root Mean Squared Error, RMSE
         self.Rsquared = 1.0 - (np.var(absError) / np.var(force))
-        
+
         # Get goodness of fit params
         self.chisq = self.get_chisq(indentation, force, sample_height)
         self.redchi = self.get_red_chisq(indentation, force, sample_height)
 
     def eval(self, indentation, sample_height=None):
         return self.model(indentation, self.delta0, self.E0, self.f0, self.slope, sample_height)
-    
+
     def get_residuals(self, indentation, force,  sample_height=None):
         return force - self.eval(indentation, sample_height)
 
     def get_chisq(self, indentation, force, sample_height=None):
-        
-        #residuals = self.get_residuals(indentation, force, sample_height)
+
+        # residuals = self.get_residuals(indentation, force, sample_height)
         # Only calculate where force is not zero
-        #weighted_res = (residuals[force != 0] ** 2) / force[force != 0]
+        # weighted_res = (residuals[force != 0] ** 2) / force[force != 0]
         # and getting a sum post filtering out non-finite values (e.g., NaN, inf)
-        #return np.sum(weighted_res[np.isfinite(weighted_res)])
-        
+        # return np.sum(weighted_res[np.isfinite(weighted_res)])
+
         a = (self.get_residuals(indentation, force, sample_height)**2/force)
 
         return np.sum(a[np.isfinite(a)])
-    
+
     def get_red_chisq(self, indentation, force, sample_height=None):
         return self.get_chisq(indentation, force, sample_height) / self.n_params
-    
+
     def fit_report(self):
         print(f"""
         # Fit parameters
@@ -211,4 +227,4 @@ class HertzModel:
         Chisq: {self.chisq}\n
         RedChisq: {self.redchi}\n
         """
-        )
+              )
--- PyFMRheo_DyNaMo/src/pyfmrheo/models/ting.py
+++ PyFMRheo_DyNaMo/src/pyfmrheo/models/ting.py
@@ -5,13 +5,16 @@ from lmfit import Model, Parameters
 from .geom_coeffs import get_coeff
 from ..utils.signal_processing import numdiff, smooth, hyp2f1_apprx
 
+
 class TingModel:
     def __init__(self, ind_geom, tip_param, modelFt) -> None:
         # Tip geomtry params
         self.ind_geom = ind_geom         # No units
-        self.tip_parameter = tip_param   # If radius units is meters, If half angle units is degrees
+        # If radius units is meters, If half angle units is degrees
+        self.tip_parameter = tip_param
         self.modelFt = modelFt
-        self.fit_method = 'leastsq' #leastsq is the default, Least-Squares minimization, using Trust Region Reflective method
+        # leastsq is the default, Least-Squares minimization, using Trust Region Reflective method
+        self.fit_method = 'leastsq'
         # Compiutation params
         self.fit_hline_flag = False
         self.apply_bec_flag = False
@@ -52,13 +55,16 @@ class TingModel:
         self.smooth_w = None
         # Moximum indentation time
         self.idx_tm = None
-    
+        self.fit_time = []
+        self.fit_force = []
+        self.fit_ind = []
 
     def build_params(self):
         params = Parameters()
         params.add('E0', value=self.E0_init, min=self.E0_min, max=self.E0_max)
         params.add('tc', value=self.tc_init, min=self.tc_min, max=self.tc_max)
-        params.add('betaE', value=self.betaE_init, min=self.betaE_min, max=self.betaE_max)
+        params.add('betaE', value=self.betaE_init,
+                   min=self.betaE_min, max=self.betaE_max)
         params.add('F0', value=self.F0_init, min=self.F0_min, max=self.F0_max)
         return params
 
@@ -66,68 +72,77 @@ class TingModel:
         # TO DO: ADD REFERENCE!!!
         # Paraboloidal geometry
         if model_probe == 'paraboloid':
-            Cp=1/geom_coeff
-            Ftp=3/2*v0t**(3/2)*E0*t0**betaE*np.sqrt(np.pi)*np.array(gamma(1-betaE), dtype=float)/(Cp*2*np.array(gamma(5/2-betaE), dtype=float))*ttc**(3/2-betaE)
-            if np.abs(v0r-v0t)/v0t<0.01:
-                Frp=3/Cp*E0*v0**(3/2)*t0**betaE/(3+4*(betaE-2)*betaE)*t1**(-1/2)*(trc-t1)**(1-betaE)*\
+            Cp = 1/geom_coeff
+            Ftp = 3/2*v0t**(3/2)*E0*t0**betaE*np.sqrt(np.pi)*np.array(gamma(1-betaE),
+                                                                      dtype=float)/(Cp*2*np.array(gamma(5/2-betaE), dtype=float))*ttc**(3/2-betaE)
+            if np.abs(v0r-v0t)/v0t < 0.01:
+                Frp = 3/Cp*E0*v0**(3/2)*t0**betaE/(3+4*(betaE-2)*betaE)*t1**(-1/2)*(trc-t1)**(1-betaE) *\
                     (-trc+(2*betaE-1)*t1+trc*hyp2f1_apprx(1, 1/2-betaE, 1/2, t1/trc))
             else:
-                Frp=3/Cp*E0*v0t**(3/2)*t0**betaE/(3+4*(betaE-2)*betaE)*t1**(-1/2)*(trc-t1)**(1-betaE)*\
+                Frp = 3/Cp*E0*v0t**(3/2)*t0**betaE/(3+4*(betaE-2)*betaE)*t1**(-1/2)*(trc-t1)**(1-betaE) *\
                     (-trc+(2*betaE-1)*t1+trc*hyp2f1_apprx(1, 1/2-betaE, 1/2, t1/trc))
             return np.r_[Ftp, Frp]
         # Conical/Pyramidal geometry
         elif model_probe in ('cone', 'pyramid'):
-            Cc=1/geom_coeff
-            if np.abs(v0r-v0t)/v0t<0.01:
-                Ftc=2*v0**2*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*ttc**(2-betaE)
-                Frc=-2*v0**2.*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*((trc-t1)**(1-betaE)*(trc+(1-betaE)*t1)-\
-                    trc**(1-betaE)*(trc))
+            Cc = 1/geom_coeff
+            if np.abs(v0r-v0t)/v0t < 0.01:
+                Ftc = 2*v0**2*E0*t0**betaE/Cc / \
+                    (2-3*betaE+betaE**2)*ttc**(2-betaE)
+                Frc = -2*v0**2.*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*((trc-t1)**(1-betaE)*(trc+(1-betaE)*t1) -
+                                                                      trc**(1-betaE)*(trc))
             else:
-                Ftc=2*v0t**2*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*ttc**(2-betaE)
-                Frc=-2*v0t**2*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*((trc-t1)**(1-betaE)*(trc+(1-betaE)*t1)-\
-                    trc**(1-betaE)*(trc))
+                Ftc = 2*v0t**2*E0*t0**betaE/Cc / \
+                    (2-3*betaE+betaE**2)*ttc**(2-betaE)
+                Frc = -2*v0t**2*E0*t0**betaE/Cc/(2-3*betaE+betaE**2)*((trc-t1)**(1-betaE)*(trc+(1-betaE)*t1) -
+                                                                      trc**(1-betaE)*(trc))
             return np.r_[Ftc, Frc]
-    
+
     def SolveNumerical(self, delta, time_, geom_coeff, geom_exp, v0t, v0r, E0, betaE, F0, vdrag, smooth_w, idx_tm, idxCt, idxCr):
         delta0 = delta - delta[idxCt[0]]
         delta_Uto_dot = np.zeros(len(delta0))
-        A = smooth(np.r_[numdiff(delta0[idxCt]**geom_exp), numdiff(delta0[idxCr[0]:]**geom_exp)], smooth_w)
+        A = smooth(np.r_[numdiff(delta0[idxCt]**geom_exp),
+                   numdiff(delta0[idxCr[0]:]**geom_exp)], smooth_w)
         if len(A) < len(delta_Uto_dot[idxCt[0]:]):
             A = np.append(A, A[-1])
         delta_Uto_dot[idxCt[0]:] = A
         delta_dot = np.zeros(len(delta0))
-        B = smooth(np.r_[numdiff(delta0[idxCt]), numdiff(delta0[idxCr[0]:])], smooth_w)
+        B = smooth(np.r_[numdiff(delta0[idxCt]),
+                   numdiff(delta0[idxCr[0]:])], smooth_w)
         if len(B) < len(delta_Uto_dot[idxCt[0]:]):
             B = np.append(B, B[-1])
         delta_dot[idxCt[0]:] = B
         Ftc = np.zeros(len(idxCt))
         for i in range(len(idxCt)):
             idx = idxCt[0] + np.arange(1, i)
-            Ftc[i] = geom_coeff * E0 * np.sum(delta_Uto_dot[idx]*np.flipud(time_[idx])**(-betaE))
+            Ftc[i] = geom_coeff * E0 * \
+                np.sum(delta_Uto_dot[idx]*np.flipud(time_[idx])**(-betaE))
         idx_min_phi0 = np.zeros(len(idxCt))
         Frc = np.zeros(len(idxCt))
         for j in range(idx_tm+1, idx_tm+len(idxCt)):
-            phi0 = np.flipud(np.cumsum(np.flipud(time_[j-1:idxCt[1]-1:-1]**(-betaE)*delta_dot[idxCt[1]+1:j+1]), axis=0))
+            phi0 = np.flipud(np.cumsum(np.flipud(
+                time_[j-1:idxCt[1]-1:-1]**(-betaE)*delta_dot[idxCt[1]+1:j+1]), axis=0))
             phi0 = phi0[:len(idxCt)]
             idx_min_phi0 = np.argmin(np.abs(phi0))
             idxCr0 = np.arange(j+1, j-idx_min_phi0+1, -1)
             t10 = time_[idxCr0]
             idx = np.arange(idxCt[0]+1, idxCt[0]+idx_min_phi0+1)
-            Frc[j-idx_tm-1] = geom_coeff * E0 * np.trapz(delta_Uto_dot[idx]*t10**(-betaE))
+            Frc[j-idx_tm-1] = geom_coeff * E0 * \
+                np.trapz(delta_Uto_dot[idx]*t10**(-betaE))
         return np.r_[Ftc, Frc]
-    
+
     def model(
         self, time, E0, tc, betaE, F0, t0, F, delta, modelFt, vdrag,
         idx_tm=None, smooth_w=None, v0t=None, v0r=None
-        ):
+    ):
         # Get indenter shape coefficient and exponent
-        geom_coeff, geom_exp = get_coeff(self.ind_geom, self.tip_parameter, self.poisson_ratio)
+        geom_coeff, geom_exp = get_coeff(
+            self.ind_geom, self.tip_parameter, self.poisson_ratio)
         # Shift time using t at contact.
-        time=time-tc
+        time = time-tc
         # print(f'time --> {time}')
         # print(f'tc --> {tc}')
         # Compute deltat.
-        deltat=time[1]-time[0]
+        deltat = time[1]-time[0]
         # print(f'idx_tm --> {idx_tm}')
         # If no t max index is given search the index of F max.
         if idx_tm is None:
@@ -136,18 +151,18 @@ class TingModel:
         tm = time[idx_tm]
         # print(f'tm --> {tm}')
         # Determine non contact trace region.
-        idxNCt=np.where(time<0)[0]
+        idxNCt = np.where(time < 0)[0]
         # Determine contact trace region
-        idxCt=np.where(time>=0)[0]
+        idxCt = np.where(time >= 0)[0]
         # Get indices corresponding to contact trace region.
         # Including t max.
         idxCt = np.arange(idxCt[0], idx_tm + 1)
         # Determine contact time trace.
-        ttc=time[idxCt]
+        ttc = time[idxCt]
         if v0t is None and self.v0t is None:
             # Define range to compute trace speed.
             # Including t max.
-            range_v0t=np.arange((idx_tm-int(len(ttc)*3/4)), idx_tm)
+            range_v0t = np.arange((idx_tm-int(len(ttc)*3/4)), idx_tm)
             # Fit 1 degree polynomial (x0 + m) to trace and retrace for determining
             # the corresponding speeds (x0)
             v0t = np.polyfit(time[range_v0t], delta[range_v0t], 1)[0]
@@ -157,40 +172,40 @@ class TingModel:
         if v0r is None and self.v0r is None:
             # Define range to compute retrace speed.
             # Excluding t max.
-            range_v0r=np.arange(idx_tm+2, (idx_tm+1+int(len(ttc)*3/4)))
+            range_v0r = np.arange(idx_tm+2, (idx_tm+1+int(len(ttc)*3/4)))
             # Fit 1 degree polynomial (x0 + m) to trace and retrace for determining
-            # the corresponding speeds (x0) 
+            # the corresponding speeds (x0)
             v0r = -1 * np.polyfit(time[range_v0r], delta[range_v0r], 1)[0]
             self.v0r = v0r
         elif v0r is None and self.v0r is not None:
             v0r = self.v0r
         # Compute mean speed.
-        v0=(v0r+v0t)/2
+        v0 = (v0r+v0t)/2
         # Compute retrace contact time.
         # TO DO: ADD REFERENCE TO ARTICLE!!!!
-        tcr=(1+v0r/v0t)**(1/(1-betaE))/((1+v0r/v0t)**(1/(1-betaE))-1)*tm
+        tcr = (1+v0r/v0t)**(1/(1-betaE))/((1+v0r/v0t)**(1/(1-betaE))-1)*tm
         # If the retrace contact time is smaller than t max,
         # define the end of the contact retrace region as 3 times t max.
-        if not tcr<tm:
-            idxCr=np.where((time>tm) & (time<=tcr))[0]
+        if not tcr < tm:
+            idxCr = np.where((time > tm) & (time <= tcr))[0]
         else:
-            idxCr=np.where((time>tm) & (time<=3*tm))[0]
+            idxCr = np.where((time > tm) & (time <= 3*tm))[0]
         # print(idxCr)
         # Define in contact retrace region.
-        trc=time[idxCr]
+        trc = time[idxCr]
         # Compute t1
         # TO DO: ADD REFERENCE TO ARTICLE!!!!
-        t1=trc-(1+v0r/v0t)**(1/(1-betaE))*(trc-tm)
+        t1 = trc-(1+v0r/v0t)**(1/(1-betaE))*(trc-tm)
         # Select only the values larger than 0 of t1.
-        t1=t1[t1>0]
+        t1 = t1[t1 > 0]
         # Select the region of retrace time where t1 is larger than 0.
-        trc=trc[t1>0]
+        trc = trc[t1 > 0]
         # Select the retrace contact indices corresponding to the retrace
-        # time region where t1 is larger than 0. 
-        idxCr=idxCr[:len(trc)]
+        # time region where t1 is larger than 0.
+        idxCr = idxCr[:len(trc)]
         # print(idxCr)
         # Assign the value of F0 to the non contact region.
-        FtNC=F0*np.ones(idxNCt.size)
+        FtNC = F0*np.ones(idxNCt.size)
         # Compute Force according to the selected mode:
         if modelFt == 'analytical':
             FJ = self.SolveAnalytical(
@@ -201,45 +216,50 @@ class TingModel:
                 delta, time, geom_coeff, geom_exp, v0t, v0r, E0, betaE, F0, vdrag, smooth_w, idx_tm, idxCt, idxCr
             )
         else:
-            print(f'The modelFt {modelFt} is not supported. Current valid modelFt: analytical, numerical.')
+            print(
+                f'The modelFt {modelFt} is not supported. Current valid modelFt: analytical, numerical.')
         # Determine non contact retrace region.
-        idxNCr=np.arange((len(FJ)+len(FtNC)+1),len(delta)+1)
+        idxNCr = np.arange((len(FJ)+len(FtNC)+1), len(delta)+1)
         # Assign the value of F0 to the non contact region.
-        FrNC=F0*np.ones(idxNCr.size)
+        FrNC = F0*np.ones(idxNCr.size)
         # Concatenate non contact regions to the contact region. And return.
         return np.r_[FtNC, FJ+F0, FrNC]+smooth(numdiff(delta)*vdrag/numdiff(time), 21)
-    
+
     def fit(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
-        #self.fit_time = time;self.fit_force = F;self.fit_ind = delta
-        
+        self.fit_time = time
+        self.fit_force = F
+        self.fit_ind = delta
+
         # Define fixed params
         self.t0 = t0
         self.idx_tm = idx_tm
         self.smooth_w = smooth_w
         self.v0t = v0t
         self.v0r = v0r
-        
+
         # Define fixed params
         fixed_params = {
             't0': self.t0, 'F': F, 'delta': delta,
             'modelFt': self.modelFt, 'vdrag': self.vdrag, 'smooth_w': self.smooth_w,
             'idx_tm': self.idx_tm, 'v0t': self.v0t, 'v0r': self.v0r
         }
-        
+
         # Prepare model for fit using fixed params
         tingmodel =\
-            lambda time, E0, tc, betaE, F0: self.model(time, E0, tc, betaE, F0, **fixed_params)
-        
+            lambda time, E0, tc, betaE, F0: self.model(
+                time, E0, tc, betaE, F0, **fixed_params)
+
         tingmodelfit = Model(tingmodel)
-        
+
         # Define free params
         params = self.build_params()
-        
+
         # Do fit
         self.n_params = len(tingmodelfit.param_names)
 
-        result_ting = tingmodelfit.fit(F, params, time=time, method=self.fit_method)
-        
+        result_ting = tingmodelfit.fit(
+            F, params, time=time, method=self.fit_method)
+
         # Assign fit results to model params
         self.E0 = result_ting.best_values['E0']
         self.tc = result_ting.best_values['tc']
@@ -247,19 +267,22 @@ class TingModel:
         self.F0 = result_ting.best_values['F0']
 
         # Compute metrics
-        modelPredictions = self.eval(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
+        modelPredictions = self.eval(
+            time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
 
         absError = modelPredictions - F
 
-        self.MAE = np.mean(absError) # mean absolute error
-        self.SE = np.square(absError) # squared errors
-        self.MSE = np.mean(self.SE) # mean squared errors
-        self.RMSE = np.sqrt(self.MSE) # Root Mean Squared Error, RMSE
+        self.MAE = np.mean(absError)  # mean absolute error
+        self.SE = np.square(absError)  # squared errors
+        self.MSE = np.mean(self.SE)  # mean squared errors
+        self.RMSE = np.sqrt(self.MSE)  # Root Mean Squared Error, RMSE
         self.Rsquared = 1.0 - (np.var(absError) / np.var(F))
 
         # Get goodness of fit params
-        self.chisq = self.get_chisq(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
-        self.redchi = self.get_red_chisq(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
+        self.chisq = self.get_chisq(
+            time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
+        self.redchi = self.get_red_chisq(
+            time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
 
     def eval(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
         return self.model(
@@ -267,15 +290,16 @@ class TingModel:
             self.modelFt, self.vdrag, idx_tm, smooth_w, v0t, v0r)
 
     def get_residuals(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
-        return F - self.eval(time, F, delta, t0,idx_tm, smooth_w, v0t, v0r)
+        return F - self.eval(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)
 
     def get_chisq(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
-        a = (self.get_residuals(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r)**2/F)
+        a = (self.get_residuals(time, F, delta,
+             t0, idx_tm, smooth_w, v0t, v0r)**2/F)
         return np.sum(a[np.isfinite(a)])
-    
+
     def get_red_chisq(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
         return self.get_chisq(time, F, delta, t0, idx_tm, smooth_w, v0t, v0r) / self.n_params
-    
+
     def fit_report(self):
         print(f"""
         # Fit parameters
@@ -299,4 +323,4 @@ class TingModel:
         Chisq: {self.chisq}\n
         RedChisq: {self.redchi}\n
         """
-        )
+              )
